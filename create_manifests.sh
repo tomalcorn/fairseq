4563e9e43207b13b3bb1fdeec199a19c5be2d280
Tom Alcorn <s2517781@ed.ac.uk>
2024-09-11 12:37:25 +0100

    Update

--- create_manifests.sh
+++
@@ -1,8 +0,0 @@
-DATA_ROOT=/Users/tomalcorn/Documents/University/pg/diss/code/data/
-LANG_ID=es
-AUDIO_MANIFEST_ROOT=/Users/tomalcorn/Documents/University/pg/diss/code/data/manifests
-
-python -m examples.speech_synthesis.preprocessing.get_common_voice_audio_manifest \
-  --data-root ${DATA_ROOT} \
-  --lang ${LANG_ID} \
-  --output-manifest-root ${AUDIO_MANIFEST_ROOT} --convert-to-wav
